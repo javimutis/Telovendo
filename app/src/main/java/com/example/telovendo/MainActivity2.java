c11f588f0c28df784dbc1e0e2ac62a3a809a4f06
Moises <moises.segura94@gmail.com>
2023-03-18 11:59:23 -0300

    Segunda pantalla + boton regresar

--- app/src/main/java/com/example/telovendo/MainActivity2.java
+++ app/src/main/java/com/example/telovendo/MainActivity2.java
@@ -1,27 +1,25 @@
 package com.example.telovendo;
-
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    Button buttonIngreso;
+public class MainActivity2 extends AppCompatActivity {
+    Button buttonRegresar;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        buttonIngreso=(Button)findViewById(R.id.buttonIngreso);
-
-        buttonIngreso.setOnClickListener(new View.OnClickListener(){
+        setContentView(R.layout.activity_main2);
+        buttonRegresar=(Button)findViewById(R.id.buttonRegresar);
+        buttonRegresar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent (MainActivity.this,MainActivity2.class);
+                Intent i = new Intent (MainActivity2.this,MainActivity.class);
                 startActivity(i);
             }
         });
