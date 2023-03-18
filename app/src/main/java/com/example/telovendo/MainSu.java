a057c0aefed6da3efc6d09a91edb93aced2dcbf4
Susana Muñoz <su.munozh@gmail.com>
2023-03-18 13:03:32 -0300

    Commit de prueba

---
+++ app/src/main/java/com/example/telovendo/MainSu.java
@@ -0,0 +1,14 @@
+package com.example.telovendo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainSu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_su);
+    }
+}
